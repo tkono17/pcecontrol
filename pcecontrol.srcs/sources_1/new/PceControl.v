7f897f19a9515a30fe24f22968fdec256f3cf455
Takanori Kono (admin) <tkono@localhost.localdomain>
2022-12-23 12:06:31 +0900

    Add SimplePulser.v

---
+++ pcecontrol.srcs/sources_1/new/PceControl.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/23/2022 12:05:11 PM
+// Design Name: 
+// Module Name: PceControl
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module PceControl(
+    input CLK_IN,
+    input RESETn
+    );
+endmodule
